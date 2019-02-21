03a89fcc484dca06b49e63abcb74a7e8b7adec34
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2019-02-21 16:05:42 +0300

    Registered custom middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        // Custom
+        'admin' => \App\Http\Middleware\Administrator::class,
+        'timeout' => \App\Http\Middleware\SessionTimeout::class,
     ];
 }
